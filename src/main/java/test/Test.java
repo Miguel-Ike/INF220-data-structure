a7e833fa222da9afb9843de5122b614f9d231cf6
Miguel-Ike <miguel_angel-m@outlook.com>
2021-09-05 21:24:25 -0400

    Update Test.java
    Pruebas de TDA Cola Generica

--- src/main/java/test/Test.java
+++ src/main/java/test/Test.java
@@ -7,6 +7,7 @@ package test;
 
 import business_layer.others.Person;
 import business_layer.simple.generic.lists.List;
+import business_layer.simple.generic.queue.Queue;
 import java.time.LocalDate;
 import java.time.Month;
 
@@ -39,13 +40,24 @@ public class Test {
         System.out.println(list);
     }
 
+    public void testQueue() {
+        Queue<Person> queue = new Queue<>();
+        System.out.println(queue);
+        queue.enqueue(new Person(9791996, "Miguel", "Martinez", LocalDate.of(1995, Month.SEPTEMBER, 3)));
+        queue.enqueue(new Person(9691996, "Katherine", "Valverde", LocalDate.of(1995, Month.SEPTEMBER, 8)));
+        queue.enqueue(new Person(9891996, "Johana", "Huiza", LocalDate.of(1995, Month.SEPTEMBER, 4)));
+        System.out.println(queue);
+        System.out.println(queue);
+    }
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
         Test test = new Test();
-        test.testList();
+//        test.testList();
+        test.testQueue();
     }
 
 }
